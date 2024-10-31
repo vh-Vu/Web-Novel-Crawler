bc3ce8575dbcff94e5d9641eed9e3cb021c2fa2f
Hardiness7387 <huynh-vu@outlook.com>
2024-11-01 03:55:49 +0700

    add header extension and hand image

--- extension/js/contentSite.js
+++ extension/js/contentSite.js
@@ -1,10 +1,1 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "getDOM") {
-        const title = document.title;
-        const url = window.location.href;
-        const bodyInnerHTML = document.body.innerHTML; // Lấy toàn bộ innerHTML của body
-
-        // Gửi phản hồi lại popup script với các thông tin cần thiết
-        sendResponse({ title, url, bodyInnerHTML });
-    }
-});
+window.addEventListener

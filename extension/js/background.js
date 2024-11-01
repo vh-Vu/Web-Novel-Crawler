c121385b051894a947802c320de632cb5986509d
Hardiness7387 <huynh-vu@outlook.com>
2024-11-01 13:24:42 +0700

    add bussiness layer

---
+++ extension/js/background.js
@@ -0,0 +1,7 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === "sendH1Content") {
+        // Gửi thông điệp đến popup
+        console.log(message.content)
+       sendResponse({farewell: "goobye"});
+    }
+});

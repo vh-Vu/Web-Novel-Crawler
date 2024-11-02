474e3510cfba089f9e5a51671997a2425772c2ea
Hardiness7387 <huynh-vu@outlook.com>
2024-11-03 03:44:37 +0700

    optimize code and store and load data

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,39 +1,28 @@
 importScripts('cookiesExtractor.js');
 importScripts('APIWorker.js');
-let host;
-let slug;
+importScripts('extentionTool.js');
 
 
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "user-action") {
-        console.log("host la", message.host);
-        console.log("path la", message.content);
-        let storageHost = chrome.storage.local.get("host");
-        if(storageHost!==message.host){
-            chrome.storage.local.set({"host":message.host});
-        }
+        chrome.storage.local.set({"host":message.host});
         slug = getSlug(message.content);
-        let storageSlug = chrome.storage.local.get("slug");
-        if (slug !== storageSlug){
-            chrome.storage.local.set({ slug: slug });
-        }
-    }
-    
-});
+        chrome.storage.local.set({ slug: slug });
+}});
 
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "getNameAndTotalChapter") {
         (async () => {
             try {
-                sendResponse(await getNovelInfo(slug));
+                slug = await chrome.storage.local.get("slug");
+                sendResponse(await getNovelInfo(slug.slug));
             } catch (error) {
                 console.error("Error fetching novel info:", error);
                 sendResponse({ error: error.message });
             }
         })();
-
         return true;
     }
     
@@ -44,7 +33,6 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (message.action === "getFiveNewestChapters") {
         (async () => {
             try {
-                console.log("FUCK");
                 sendResponse(await getFiveNewestChapters(message.storyId));
             } catch (error) {
                 console.error("Error fetching novel info:", error);

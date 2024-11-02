e3d0c4aa7b4e508ed6a5162c27e9f27e21af7650
Hardiness7387 <huynh-vu@outlook.com>
2024-11-02 13:28:07 +0700

    add observer service to watching DOM changing

--- extension/js/contentSite.js
+++ extension/js/contentSite.js
@@ -1,1 +1,45 @@
-window.addEventListener
+const WEBSITE_IDENTIFY = {
+    BACH_NGOC_SACH_VIP : 1
+}
+
+const SUPPORTED_WEBSITE = {
+    "bachngocsach.net.vn" : WEBSITE_IDENTIFY.BACH_NGOC_SACH_VIP,
+    "bachngocsach.app": WEBSITE_IDENTIFY.BACH_NGOC_SACH_VIP}
+
+let lastUrl = location.href;
+
+// obsever duty is watching DOM content changing, this one for only BachNgocSach
+const observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+        if (lastUrl!= location.href) { //When DOM content had changed, will checking url changing.
+            lastUrl = location.href;
+            sendToWorker();
+        }
+    });
+});
+    
+const host = this.location.host;
+
+function weAreChecking(){
+    switch(SUPPORTED_WEBSITE[host]){
+    case 1:
+        observer.observe(document.body, { childList: true, subtree: true }); // Starting obsever service
+        sendToWorker();
+        break;
+    default:
+        console.log("Not Supported");
+    }
+}
+
+function refeshGetContent(event){
+    weAreChecking();
+}
+refeshGetContent();
+//This event for Back and Forward for future
+//window.addEventListener("popstate", refeshGetContent)
+
+//Send host and pathname to background
+function sendToWorker(){
+    chrome.runtime.sendMessage({ host: SUPPORTED_WEBSITE[host], content: location.pathname});
+}
+

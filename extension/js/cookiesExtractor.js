bae371188dbf6a2c3dfe86042a305a0ad4363cf4
Hardiness7387 <huynh-vu@outlook.com>
2024-11-02 20:46:44 +0700

    getNovelInfo

--- extension/js/cookiesExtractor.js
+++ extension/js/cookiesExtractor.js
@@ -2,3 +2,19 @@
 chrome.cookies.getAll({ domain: "bachngocsach.net.vn" }, (cookies) => {
     console.log("Cookies for bachngocsach.net.vn:", cookies);
 });
+
+
+async function getNovelInfo(slug,WEB_API="https://ngocsach.com/api/story-by-slug/"){
+    try{
+        const request = await fetch(`${WEB_API}${slug}`);
+        if(request.status===200){
+            let responce = await request.json();
+            return responce.name, responce.chapter_count;
+        }else{
+            throw new Exception("Cannot get API form sever try it later")
+        }
+    }catch{
+        throw new Error("Vui lòng báo cho lập trình viên ở email dưới footer");
+    }
+
+}

197871d369b644ed2082ac9acded3edb44089092
vh-Vu <huynh-vu@outlook.com>
2024-11-09 00:12:01 +0700

    add function getID to claim novelID

--- extension/js/DaoQuanCollecter.js
+++ extension/js/DaoQuanCollecter.js
@@ -1,7 +1,6 @@
-const storyPath = "/doc-truyen/";
 const img = "https://img.daoquan.vn/get/images/doctruyen/images/20220405/web.png"
 async function DaoQuangetNameAndTotalChapter(pathName){
-    console.log(getSlugNameorID(pathName,storyPath,true));
+    console.log(GetId(pathName));
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -10,3 +9,17 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         // Xử lý dữ liệu hoặc lưu trữ vào storage nếu cần
     }
 });
+
+
+function GetId(pathName){
+    const storyPath = "/doc-truyen/";
+    if(pathName.includes(storyPath)){
+        const novelPath = pathName.slice(storyPath.length);
+        const indexOfForwardSlash = novelPath.indexOf("/");
+        return novelPath.slice(indexOfForwardSlash+1);
+    }
+    const regex = /^\/([a-zA-Z0-9\-]+)\/(\d+)\/1\/chuong-(\d+)$/;
+    const match = pathName.match(regex);
+    if(match) return match[2];
+    return null;
+}

65960ec13558e56670dd30c6b5533103dd4a5b6f
vh-Vu <huynh-vu@outlook.com>
2024-12-20 10:43:47 +0700

    change config to json

--- extension/js/config.js
+++ extension/js/config.js
@@ -1,12 +1,12 @@
-// config.js
-const WEBSITE_IDENTIFY = {
-    BACH_NGOC_SACH_VIP: 1,
-    DAO_QUAN: 2
-};
-
-const SUPPORTED_WEBSITE = {
-    "bnsvip.com": 1,
-    "bachngocsach.app": 1,
-    "daoquan.vn": 2,
-    // Có thể thêm các website khác vào đây khi cần
-};
+{
+    "WEBSITE_IDENTIFY": {
+      "BACH_NGOC_SACH_VIP": 1,
+      "DAO_QUAN": 2
+    },
+    "SUPPORTED_WEBSITE": {
+      "bnsvip.com": 1,
+      "bachngocsach.app": 1,
+      "daoquan.vn": 2
+    }
+  }
+  

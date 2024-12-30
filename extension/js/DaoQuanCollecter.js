f31a50dcff202b356b2231aed958874afa810fca
vh-Vu <huynh-vu@outlook.com>
2024-12-31 01:39:12 +0700

    move fetch every site from background to DaoQuan service, that was where it belong

--- extension/js/DaoQuanCollecter.js
+++ extension/js/DaoQuanCollecter.js
@@ -48,19 +48,12 @@ async function DQGetNovelInfo(id) {
 }
 
 
-//Biến /n thành thẻ <p></p>
-function formatDescription(description) {
-    let paragraphs = description.split('\n');
-    let formattedDescription = paragraphs.map(paragraph => {
-        return `<p>${paragraph.replace(/\n/g, ' ')}</p>`;
-    }).join('');
-    return formattedDescription;
-}
+
+
 
 
 async function DaoQuanGetTotalChapters(id){
     try{
-        console.log(`${E5_CHAPTERS_API(id)}`);
     const request = await fetch(`${E5_CHAPTERS_API(id)}`);
     if(request.status !== 200 ) throw new Error("Trang web đang bị lỗi, vui lòng thử lại sau");
         const response = await request.json();
@@ -80,5 +73,14 @@ async function DaoQuanGetChapterContent(ChapterID){
         }
     catch {
         throw new Error("Vui lòng báo cho lập trình viên ở email dưới footer");
+    }
+}
+
+async function DaoQuanAddChapterProcess(novel,ebook){
+    const totalChapter = await DaoQuanGetTotalChapters(novel.id);
+    for (let i = 0; i < totalChapter.length; i++) {
+        const chapterContent = await DaoQuanGetChapterContent(totalChapter[i].id);
+        UpdateProgress(totalChapter[i].number,totalChapter[i].name,i+1,novel.totalChapter);
+        ebook.addChapter(totalChapter[i].number,totalChapter[i].name,chapterContent);
     }
 }
